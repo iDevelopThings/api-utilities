baf6eb6881eef9b3b6b8b519c4736111ec94511b
Sam Parton <sam@idevelopthings.com>
2021-11-23 21:54:08 +0000

    Some more messed up testing

--- build.sh
+++ build.sh
@@ -4,27 +4,31 @@ rm -rf dist-node
 
 echo ">> Building node version"
 
-./node_modules/.bin/tsdx build --tsconfig tsconfig.node.json --target node
+#./node_modules/.bin/tsdx build --tsconfig tsconfig.node.json --target node
 
-mv dist/ dist-node/
+./node_modules/.bin/tsc --build tsconfig.node.json
+
+#mv dist/ dist-node/
 
 echo ">> Building web version"
 
-./node_modules/.bin/tsdx build --tsconfig tsconfig.json --target web
+#./node_modules/.bin/tsdx build --tsconfig tsconfig.json --target web
 
-mv dist/ dist-web/
+./node_modules/.bin/tsc --build tsconfig.web.json
+
+#mv dist/ dist-web/
 
 echo ">> Generating types"
 
 ./node_modules/.bin/tsc --build tsconfig.declarations.json
 
-echo ">> Restructuring files..."
+#echo ">> Restructuring files..."
 
-mkdir dist
+#mkdir dist
 
-mv dist-web/ dist/web/
-mv dist-node/ dist/node/
-mv dist-types/ dist/types/
+#mv dist-web/ dist/web/
+#mv dist-node/ dist/node/
+#mv dist-types/ dist/types/
 
 
 
